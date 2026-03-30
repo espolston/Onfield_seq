581a457c387ca687f34e8f16239ba22669d06302
espolston <espolston@uchicago.edu>
2026-03-30 13:49:23 -0500

    Create makebed.R

---
+++ makebed.R
@@ -0,0 +1,26 @@
+bed <- read.table("/Users/libbypolston/Desktop/UChicago/Kreiner_lab/Coding/Rotation_Winter2025/Results/dcgm_sig.bed")
+
+order <- c("Scaffold_2",
+           "Scaffold_3",
+           "Scaffold_5",
+           "Scaffold_11",
+           "Scaffold_6",
+           "Scaffold_4",
+           "Scaffold_8",
+           "Scaffold_12",
+           "Scaffold_7",
+           "Scaffold_10",
+           "Scaffold_1",
+           "Scaffold_13",
+           "Scaffold_9",
+           "Scaffold_16",
+           "Scaffold_15",
+           "Scaffold_14")
+
+bed_sort <- bed %>% subset(V1 == order[1])
+
+for(i in 2:16){
+  bed_sort <- add_row(bed_sort, bed %>% subset(V1 == order[i]))  
+}
+
+write.table(bed_sort, file = "/Users/libbypolston/Desktop/UChicago/Kreiner_lab/Coding/Onfield_sampling/dcgm_sig_ordered.bed", row.names = F, quote = F)
